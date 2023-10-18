70ec66336b1c0cdbcdab3fc04b4c39b992cb3146
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-10-18 08:02:10 -0300

    Atualizei script.js da aula 1

---
+++ Apêndices do Curso/ES6/Aula-01/scripts.js
@@ -0,0 +1,22 @@
+// 1- var, let e const
+
+var x = 10
+var y = 10
+
+if (y > 30) {
+    var x = 5
+    console.log(x)
+}
+console.log(x)
+
+
+
+let a = 10
+let b = 10
+
+if (b > 30) {
+    let a = 5
+    console.log(a)
+}
+
+console.log(a)
