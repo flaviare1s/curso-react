34c66c0e41e52f7d608f90da84697bac0835ec2f
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-19 09:06:13 -0300

    Importei imagem da pasta assets

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 
+import City from "./assets/city.jpg"
+
 function App() {
   return (
     <div className='App'>
@@ -7,6 +9,11 @@ function App() {
       {/* Imagem em public */}
       <div>
         <img src="/img1.jpg" alt="Paisagem" />
+      </div>
+      {/* Imagem em assets */}
+      <img src={City} alt="Cidade" />
+      <div>
+
       </div>
     </div>
   )
