317ebbdd1cb055d39567bf934faf90bff48cb246
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-19 09:01:00 -0300

    Adicionei uma imagem da pasta public

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -1,23 +1,13 @@
-import { useState } from 'react'
-import { FirstComponent } from './components/FirstComponent'
-import { TemplateExpressions } from './components/TemplateExpressions'
-
 import './App.css'
-import { MyComponent } from './components/MyComponent'
-import { Events } from './components/Events'
-import { Challenge } from './components/Challenge';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='App'>
-      <h1>Fundamentos do React</h1>
-      <FirstComponent />
-      <TemplateExpressions />
-      <MyComponent />
-      <Events />
-      <Challenge />
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem" />
+      </div>
     </div>
   )
 }
