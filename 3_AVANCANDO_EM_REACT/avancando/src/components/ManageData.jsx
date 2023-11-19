0fc8d98f49ce37806ce42e0436c51a5b38da02a9
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-19 11:23:55 -0300

    Aprendendo a usar hooks

--- 3_AVANCANDO_EM_REACT/avancando/src/components/ManageData.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ManageData.jsx
@@ -1,9 +1,22 @@
-
+import { useState } from "react"
 
 export const ManageData = () => {
+    let someData = 10
+
+    const [number, setNumber] = useState(15)
+
+    console.log(number)
+
   return (
     <div>
-      teste
+      <div>
+        <p>Valor: {someData}</p>
+        <button onClick={() => (someData = 15)}>Alterar variável</button>
+      </div>
+      <div>
+        <p>Valor: {number}</p>
+        <button onClick={() => setNumber(25)}>Mudar o state</button>
+      </div>
     </div>
   )
 }
