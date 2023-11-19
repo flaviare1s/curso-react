465fcce61df1f6aad33147ce0391df7dbabee3ed
Flávia Reis <143637150+flaviare1s@users.noreply.github.com>
2023-11-19 10:57:53 -0300

    Criei um novo componente

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -1,20 +1,23 @@
 import './App.css'
 
 import City from "./assets/city.jpg"
+import { ManageData } from './components/ManageData'
 
 function App() {
   return (
     <div className='App'>
       <h1>Avançando em React</h1>
-      {/* Imagem em public */}
+      
       <div>
+        {/* Imagem em public */}
         <img src="/img1.jpg" alt="Paisagem" />
       </div>
-      {/* Imagem em assets */}
-      <img src={City} alt="Cidade" />
+     
       <div>
-
+         {/* Imagem em assets */}
+        <img src={City} alt="Cidade" />
       </div>
+      < ManageData />
     </div>
   )
 }
---
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/ManageData.jsx
@@ -0,0 +1,11 @@
+
+
+export const ManageData = () => {
+  return (
+    <div>
+      teste
+    </div>
+  )
+}
+
+
