e6fe13a1ff90369b28b07de5a532c7310254e4fd
Flávia Reis <flaviarster@gmail.com>
2023-11-20 13:53:57 -0300

    Criei ShowUserName.jsx para iniciar os estudos de PROPS.

--- 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/App.jsx
@@ -4,6 +4,7 @@ import City from "./assets/city.jpg"
 import { ConditionalRender } from './components/ConditionalRender'
 import { ListRender } from './components/ListRender'
 import { ManageData } from './components/ManageData'
+import { ShowUserName } from './components/ShowUserName'
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       < ManageData />
       < ListRender />
       < ConditionalRender />
+      < ShowUserName name="Matheus" />
     </div>
   )
 }
