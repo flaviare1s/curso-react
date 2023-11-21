cbe4dc062d85ecee07d75efcb2c81885b793d7ca
Flávia Reis <flaviarster@gmail.com>
2023-11-21 10:24:47 -0300

    Criei Component.jsx e Component.css

--- 4_CSS_REACT/src/App.jsx
+++ 4_CSS_REACT/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { MyComponent } from './components/MyComponent'
 
 export function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
+      {/* CSS global*/}
       <h1>React com CSS</h1>
+      
+      {/* CSS de componente*/}
+      <MyComponent />
+      <p>Este parágrafo é do AppComponent</p>
+      <p className="my-comp-paragraphy">Esse também é do componente</p>
+      
     </div>
   )
 }
---
+++ 4_CSS_REACT/src/components/MyComponent.jsx
@@ -0,0 +1,11 @@
+import "./MyComponent.css"
+
+export const MyComponent = () => {
+  return (
+    <div>
+        <h1>CSS de Componente</h1>
+        <p>Este é o parágrafo do componente.</p>
+    </div>
+  )
+}
+
