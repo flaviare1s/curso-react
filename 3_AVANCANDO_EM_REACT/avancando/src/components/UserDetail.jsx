bf0d75f22cc74ab9d6cca575f8e3d58ff6f81782
Flávia Reis <flaviarster@gmail.com>
2023-11-21 09:40:51 -0300

    Concluí o desafio da Unidade 3

--- 3_AVANCANDO_EM_REACT/avancando/src/components/UserDetail.jsx
+++ 3_AVANCANDO_EM_REACT/avancando/src/components/UserDetail.jsx
@@ -2,12 +2,24 @@ import { useState } from "react"
 
 export const UserDetail = () => {
     const pessoas = [
-        {nome: "Tiago", idade: 20, profissao: "estudante"},
+        {nome: "Tiago", idade: 17, profissao: "estudante"},
         {nome: "Pedro", idade: 25, profissao: "vendedor"},
         {nome: "Paula", idade: 30, profissao: "médica"}
     ]
   return (
-    <div>UserDetail</div>
+    <div>
+        {pessoas.map((pessoa, index) => (
+            <div key={index}>
+                <p>{pessoa.nome}:</p>
+                {pessoa.idade >= 18 ? (
+                    <p>Pode tirar a carteira de habilitação!</p>
+                ) : (
+                    <p>Não pode tirar a carteira de habilitação!</p>
+                )    
+            } 
+            </div>
+        ))}
+    </div>
   )
 }
 
