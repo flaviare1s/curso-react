636a0c51f16871b513b18ba5f57df7aa4f83585b
Flávia Reis <flaviarster@gmail.com>
2023-11-21 17:20:18 -0300

    Concluí o desafio

--- ChallengeCSS/src/App.jsx
+++ ChallengeCSS/src/App.jsx
@@ -9,7 +9,9 @@ export function App() {
   return (
     <div>
       <h1>Desafio de CSS do Curso de React</h1>
-      < CarDetails brand={'Ferrari'} />
+      < CarDetails brand='Ferrari' color='Vermelho' km='0' />
+      < CarDetails brand='Volvo' color='Branco' km='0' />
+      < CarDetails brand='Renault' color='Preto' km='0' />
     </div>
   )
 }
--- ChallengeCSS/src/components/CarDetails.jsx
+++ ChallengeCSS/src/components/CarDetails.jsx
@@ -1,13 +1,13 @@
+import styles from './CarDetails.module.css'
 
-
-export const CarDetails = ({brand color km}) => {
+export const CarDetails = ({brand, color, km}) => {
 
   return (
-    <div>
-        <h2>Detalhes do carro:</h2>
-        <p>Marca: {brand}</p>
-        <p>Cor: {color}</p>
-        <p>Quilometragem: {km}</p>
+    <div className={styles.container}>
+        <h2 className={styles.title}>Detalhes do carro:</h2>
+        <p className={styles.paragraph}>Marca: {brand}</p>
+        <p className={styles.paragraph}>Cor: {color}</p>
+        <p className={styles.paragraph}>Quilometragem: {km}</p>
     </div>
   )
 }
