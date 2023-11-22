576f815c311cac136a48f8c9d8d39683a9044bf9
Flávia Reis <flaviarster@gmail.com>
2023-11-22 10:19:23 -0300

    Adicionei label envolvendo input

--- 5_FORM_EM_REACT/src/components/MyForm.jsx
+++ 5_FORM_EM_REACT/src/components/MyForm.jsx
@@ -9,6 +9,11 @@ export const MyForm = () => {
           <label htmlFor="name">Nome: </label>
           <input type="text" name="name" id="name" placeholder="Digite o seu nome" />
         </div>
+        {/* Label envolvendo input */}
+        <label>
+          <span>E-mail: </span>
+          <input type="email" name="email" id="email" placeholder='Digite o seu email' />
+        </label>
         <input type="submit" value="Enviar" />
       </form>
     </div>
