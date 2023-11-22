bd43e253cad220ddee580c9ea1ea5c915b702f27
Flávia Reis <flaviarster@gmail.com>
2023-11-22 12:57:32 -0300

    Criei o componente inicial e coloquei estilos

--- 6_SECRET_WORD/src/App.jsx
+++ 6_SECRET_WORD/src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import { StartScreen } from './components/StartScreen'
 
 export function App() {
 
   return (
    <div className='App'>
-    <h2>Secret Word</h2>
+    < StartScreen />
    </div>
   )
 }
