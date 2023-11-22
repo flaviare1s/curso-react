8d7a25fb95ff5949ee391413b90865831b839714
Flávia Reis <flaviarster@gmail.com>
2023-11-22 10:27:05 -0300

    useState para gerenciar dados

--- 5_FORM_EM_REACT/src/components/MyForm.jsx
+++ 5_FORM_EM_REACT/src/components/MyForm.jsx
@@ -1,15 +1,26 @@
+import { useState } from 'react'
 import './MyForm.css'
 
 export const MyForm = () => {
+  // 3. Gerenciamento de dados:
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+   console.log(name)
+
   return (
     <div>
-      {/* Criação de forms */}
+      {/* 1. Criação de forms: */}
       <form>
         <div>
           <label htmlFor="name">Nome: </label>
-          <input type="text" name="name" id="name" placeholder="Digite o seu nome" />
+          <input type="text" name="name" id="name" placeholder="Digite o seu nome" onChange={handleName} />
         </div>
-        {/* Label envolvendo input */}
+        {/* 2. Label envolvendo input: */}
         <label>
           <span>E-mail: </span>
           <input type="email" name="email" id="email" placeholder='Digite o seu email' />
