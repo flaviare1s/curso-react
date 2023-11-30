045a5517a71c3b8da8a0784430f37da36b92e748
Flávia Reis <flaviarster@gmail.com>
2023-11-30 12:36:20 -0300

    Atualizei App.jsx

--- 7_REQ_HTTP/src/App.jsx
+++ 7_REQ_HTTP/src/App.jsx
@@ -1,6 +1,23 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
 
 export function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(async () => {
+    
+    const res = await fetch(url)
+
+    const data = await res.json()
+
+    setProducts(data)
+
+  }, [])
+
+  console.log(products)
+
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
